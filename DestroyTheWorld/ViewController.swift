724a7ae64c2f45d7080ca30da9747d16d33400d2
Martinus Schempers <martinus@martinuss-mac.local>
2016-12-30 10:18:31 -0800

    Initial Commit

---
+++ DestroyTheWorld/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  DestroyTheWorld
+//
+//  Created by Martinus Schempers on 2016/12/30.
+//  Copyright © 2016 MartinMacDev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
